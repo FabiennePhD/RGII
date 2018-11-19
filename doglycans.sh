e0c35c83930e87f13ed7f4bce42570246b742c95
FabiennePhD <45097882+FabiennePhD@users.noreply.github.com>
2018-11-19 12:48:47 +0000

    Update doglycans.sh

--- doglycans.sh
+++ doglycans.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source activate doglycans
-export cl=F1
+export sd=F1 #side chain
 
-python /home/chem/mstscj/DOGLYCANS/doglycans/prepreader.py [-Wmdt] -f /home/chem/mstscj/DOGLYCANS/doglycans/dat/glycam06h.itp -p /home/chem/mstscj/DOGLYCANS/doglycans/dat/GLYCAM_06h.prep -s seq_file.seq -c ${cl}.pdb -o ${cl}.itp -- charmm
+python /home/chem/mstscj/DOGLYCANS/doglycans/prepreader.py [-Wmdt] -f /home/chem/mstscj/DOGLYCANS/doglycans/dat/glycam06h.itp -p /home/chem/mstscj/DOGLYCANS/doglycans/dat/GLYCAM_06h.prep -s seq_file.seq -c ${sd}.pdb -o ${sd}.itp -- charmm
